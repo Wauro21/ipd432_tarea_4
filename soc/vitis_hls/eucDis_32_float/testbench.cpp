6a01489b325085a8d3c2c172d0b5bf2160d9b3ee
Mauricio <maurio.aravena@sansano.usm.cl>
2022-04-21 12:38:01 -0400

    HLS para punto flotante

---
+++ soc/vitis_hls/eucDis_32_float/testbench.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include "eucDis32_float.h"
+#include <string>
+#define REL_TOL 1.0
+int main(void){
+  std::cout << "Running C++ Simulation!" << std::endl;
+
+  // Vectors
+  INPUT A[VECTOR_LENGTH], B[VECTOR_LENGTH];
+  OUTPUT C, D;
+  double D_temp;
+
+  // Read goldenReference
+
+  std::ifstream reference("goldenreference.csv");
+  if(!reference.is_open()){
+    std::cout << "Referencia no pudo ser abierta!" << std::endl;
+    exit(1);
+  }
+  int line_counter = 0;
+  std::string temp_line;
+  while(std::getline(reference, temp_line,',')){
+    if(line_counter == VECTOR_LENGTH){
+      break;
+    }
+    else{
+      A[line_counter] = (INPUT) std::stof(temp_line);
+      std::getline(reference, temp_line);
+      B[line_counter] = (INPUT) std::stof(temp_line);
+      D_temp += (A[line_counter] - B[line_counter])*(A[line_counter] - B[line_counter]);
+      line_counter += 1;
+    }
+  }
+  D = (OUTPUT) sqrt(D_temp);
+  reference.close();
+  // Calculate vector:
+  eucDis32_float(A,B,&C);
+  std::cout << C << std::endl;
+  // Compute error:
+  float direct_error = abs(D-C);
+  float relative_error = 100*direct_error/D;
+  std::cout << "-----------------------------------" << std::endl;
+  std::cout << "Valor esperado: " << D << std::endl;
+  std::cout << "Valor calculado: " << C << std::endl;
+  std::cout << "Diferencia directa: " << direct_error << std::endl;
+  std::cout << "Diferencia relativa: " << relative_error << std::endl;
+  std::cout << "-----------------------------------" << std::endl;
+  if(direct_error > REL_TOL){
+    std::cout << "[ERROR] FAILED! Difference exceeds tolerance" << std::endl;
+  }
+  else{
+    std::cout << "PASSED!" << std::endl;
+  }
+  std::cout << "-----------------------------------" << std::endl;
+}
