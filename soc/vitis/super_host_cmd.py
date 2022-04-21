00ca0b0735ec11d6045f2fd62152db785610c095
pabloreyesrobles <pabloreyes500@gmail.com>
2022-04-21 02:25:41 -0400

    mod main.c and python script added

---
+++ soc/vitis/super_host_cmd.py
@@ -0,0 +1,36 @@
+from regex import D
+import serial
+import argparse
+import random
+import numpy as np
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-N', nargs=1, required=True, type=int)
+ap.add_argument('-port', nargs=1, required=True)
+ap.add_argument('-seed', nargs='?', const=1, type=int, default=random.randint(0, 255))
+opt = ap.parse_args()
+
+try:
+  with serial.Serial('COM6', 115200) as ser:
+    ps_vec, pl_vec = np.zeros(1024), np.zeros(1024)
+
+    print('Started...')
+    print(f'Sending N = {opt.N[0]:d} trials, seed = {opt.seed:d}')
+    
+    ser.write(chr(opt.N[0]).encode())
+    ser.write(chr(opt.seed).encode())
+
+    for i in range(opt.N[0]):
+      a_data, b_data = ser.readline()[:-2].decode('utf-8').split(',')
+      ps_vec[i] = float(a_data)
+      pl_vec[i] = float(b_data)
+
+      print(f'Trial euc_dis #{i:d}: PS = {ps_vec[i]:f}, PL = {pl_vec[i]:f}')
+
+    error = ps_vec - pl_vec
+    sum_error = np.sum(np.abs(error))
+    print(f'Accum error = {sum_error:f}')
+
+except KeyboardInterrupt:
+  exit()
+
