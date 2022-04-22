8cefb26da37fb383166bd2be2b3edbcf38103a8b
Mauricio <maurio.aravena@sansano.usm.cl>
2022-04-22 05:01:23 -0400

    Orden final antes de merger

--- hls/utils/goldenGenerator.py
+++ hls/utils/goldenGenerator.py
@@ -1,12 +1,14 @@
 import csv
 import math
 from random import randrange
+from random import uniform
 import argparse
 # Generates a golden-reference file for testing in Vitis_hls
 
 default_file_name = "goldenreference"
 default_length = 1024
-max_number = 256
+MAX_N = 256
+MIN_N = 0
 
 def argParser():
     dsc = "This script generates a goldenreference file of 2 vectors, with 1024 random values from 0-255. The output is a csv file, by default, saved at: vitis_hls/goldenreference.csv"
@@ -14,6 +16,9 @@ def argParser():
     parser.add_argument("-oP","--OutputPath", help = "Indicates where to save the goldenreference file. By default it's saved to: vitis_hls/")
     parser.add_argument("-nf","--nameFile", help = "Select a name for the outputfile, by default it's: {}".format(default_file_name))
     parser.add_argument("-l", "--Length", help = "The length of the vectors to generate, by default it's 1024")
+    parser.add_argument("--max", help="Indicates the max number, by default its 255 (8 bit max)")
+    parser.add_argument("--min", help="Indicates the max number, by default its 0")
+    parser.add_argument("--float", action='store_true',help="Overrides default type for numbers <int> to <float>")
     args = parser.parse_args()
     if args.OutputPath:
         o_path = str(args.OutputPath)
@@ -28,17 +33,36 @@ def argParser():
         length = int(args.Length)
     else:
         length = default_length
-    return o_path, name_file, length
+
+    if args.max:
+        max_number = int(args.max)
+    else:
+        max_number = MAX_N
+
+    if args.min:
+        min_number = int(args.min)
+    else:
+        min_number = MIN_N
+
+    if args.float:
+        i_type = True # Float
+    else:
+        i_type = False # Int
+    return o_path, name_file, length, max_number, min_number, i_type
 
 def main():
-    path, name_file, length = argParser()
+    path, name_file, length, max_number, min_number, type = argParser()
     # Open file
     file = open(path+"/"+name_file+".csv","w")
     writer = csv.writer(file)
     # Generate "8 bit" values, in reality they are just integers.
     for i in range(length):
-        A = randrange(max_number)
-        B = randrange(max_number)
+        if(type):
+            A = uniform(min_number, max_number)
+            B = uniform(min_number, max_number)
+        else:
+            A = randrange(min_number,max_number)
+            B = randrange(min_number,max_number)
         row = [A,B]
         writer.writerow(row)
     # Close file
